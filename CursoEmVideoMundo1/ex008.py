7904211f66930c34fdca098c7250ced9e847f8f4
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-31 17:48:47 -0300

    ex008

--- CursoEmVideoMundo1/ex008.py
+++ CursoEmVideoMundo1/ex008.py
@@ -1,1 +1,5 @@
 # Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros
+
+valorMetros = float(input('Informe o valor em metros: '))
+print("O valor informado em centimetros é {}".format(valorMetros*100))
+print("O valor informado em milimetros é {}".format(valorMetros*1000))
