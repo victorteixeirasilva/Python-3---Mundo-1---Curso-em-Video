1be369818436e27161abb0bf60781dcb1fc0ac17
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-31 17:52:36 -0300

    ex009

--- CursoEmVideoMundo1/ex009.py
+++ CursoEmVideoMundo1/ex009.py
@@ -1,1 +1,15 @@
 # Faça um programa que leia um número qualquer e mostre na tela a sua tabuada.
+
+number = int(input("Informe um número: "))
+print("*** TABUADA ***")
+print("{} X 1 = {}".format(number, number * 1))
+print("{} X 2 = {}".format(number, number * 2))
+print("{} X 3 = {}".format(number, number * 3))
+print("{} X 4 = {}".format(number, number * 4))
+print("{} X 5 = {}".format(number, number * 5))
+print("{} X 6 = {}".format(number, number * 6))
+print("{} X 7 = {}".format(number, number * 7))
+print("{} X 8 = {}".format(number, number * 8))
+print("{} X 9 = {}".format(number, number * 9))
+print("{} X 10 = {}".format(number, number * 10))
+
