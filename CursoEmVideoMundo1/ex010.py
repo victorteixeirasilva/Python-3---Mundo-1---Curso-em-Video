1262dfdc2e2b361c059d1badd9ff6af03a1f7a57
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-31 17:57:19 -0300

    ex010

--- CursoEmVideoMundo1/ex010.py
+++ CursoEmVideoMundo1/ex010.py
@@ -1,3 +1,7 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 
 # Considere US$ 1.00 = R$ 3.27
+
+balance = float(input("Informe o valor que tem na sua carteira: "))
+print("Com esse valor na carteira ({}), e levando em consideração o dolar á (US$ 1.0), podemos comprar {} dolares"
+      .format(balance, balance/3.27))
