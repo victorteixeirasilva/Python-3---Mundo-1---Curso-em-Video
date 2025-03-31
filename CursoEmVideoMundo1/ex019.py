d2bf61e3e626b2e4ee659e54a73eec2f3abdb3e3
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-31 20:53:03 -0300

    ex019

--- CursoEmVideoMundo1/ex019.py
+++ CursoEmVideoMundo1/ex019.py
@@ -1,2 +1,10 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome[
 # deles e escrevendo o nome do escolhido.
+import random
+n1 = input("Primeiro aluno: ")
+n2 = input("Segundo aluno: ")
+n3 = input("Terceiro aluno: ")
+n4 = input("Quarto aluno: ")
+lista = [n1, n2, n3, n4]
+escolhido = random.choice(lista)
+print("O aluno escolhido foi {}".format(escolhido))
