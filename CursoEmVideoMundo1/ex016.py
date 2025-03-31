161613b281244d2fe6619f94fbf6b4f617df4a0c
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-31 20:27:05 -0300

    ex016

--- CursoEmVideoMundo1/ex016.py
+++ CursoEmVideoMundo1/ex016.py
@@ -2,3 +2,7 @@
 # Ex:
 # Digite um número: 6.127
 # O número 6.127 tem a parte inteira 6.
+import math
+num = float(input("Digite um valor: "))
+print("O valor digitado foi {} e sua porção inteira é {}".format(num, math.trunc(num)))
+
