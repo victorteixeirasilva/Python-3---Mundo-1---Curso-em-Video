bfb900fd57eae29c99a8228372e18a286b2f0388
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-31 20:56:13 -0300

    ex020

--- CursoEmVideoMundo1/ex020.py
+++ CursoEmVideoMundo1/ex020.py
@@ -1,2 +1,10 @@
 # O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos.
 # faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+from random import shuffle
+n1 = input("Primeiro Aluno: ")
+n2 = input("Segundo Aluno: ")
+n3 = input("Terceiro Aluno: ")
+n4 = input("Quarto Aluno: ")
+lista = [n1, n2, n3, n4]
+shuffle(lista)
+print(lista)
