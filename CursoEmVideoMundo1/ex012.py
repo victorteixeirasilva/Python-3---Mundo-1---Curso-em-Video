07200abae9fd12d45e8981cb0da9b4aea2d7be3a
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-31 18:02:50 -0300

    ex012

--- CursoEmVideoMundo1/ex012.py
+++ CursoEmVideoMundo1/ex012.py
@@ -1,1 +1,3 @@
 # Faça um programa que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+price = float(input("Informe o preço do seu produto: "))
+print("Seu produto com 5% de desconto fica {}".format(price-(price*0.05)))
