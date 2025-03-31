e60ff414a1d7ceab8a5869c5305518ce49f73330
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-31 18:04:21 -0300

    ex013

--- CursoEmVideoMundo1/ex013.py
+++ CursoEmVideoMundo1/ex013.py
@@ -1,1 +1,4 @@
 # Faça um algoritimo que leia o salário de um funcionário e mostre seu novo salário com 15% de aumento.
+
+salario = float(input("Informe o valor do salário: "))
+print("O salário {}, com 15% de aumento fica {}".format(salario, salario+(salario*0.15)))
