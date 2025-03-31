314570f5a866b92d22ed72bca1d841d1b573b7bd
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-31 20:42:24 -0300

    ex017

--- CursoEmVideoMundo1/ex016.py
+++ CursoEmVideoMundo1/ex016.py
@@ -1,7 +1,7 @@
-# Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
-# Ex:
-# Digite um número: 6.127
-# O número 6.127 tem a parte inteira 6.
+"""Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
+ Ex:
+ Digite um número: 6.127
+ O número 6.127 tem a parte inteira 6."""
 import math
 num = float(input("Digite um valor: "))
 print("O valor digitado foi {} e sua porção inteira é {}".format(num, math.trunc(num)))
--- CursoEmVideoMundo1/ex017.py
+++ CursoEmVideoMundo1/ex017.py
@@ -1,2 +1,7 @@
 # Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo,
 # calcule e mostre o comprimento da hipotenusa.
+import math
+co = float(input("Comprimento do cateto oposto: "))
+ca = float(input("Comprimento do cateto adjacente: "))
+hi = math.hypot(co, ca)
+print("A hipotenusa vai medir {:.2f}".format(hi))
