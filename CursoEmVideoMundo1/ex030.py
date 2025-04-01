37dc3872bc7c15f2961a16965cf4e8bf6bf8b41f
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 11:09:32 -0300

    ex030

--- CursoEmVideoMundo1/ex030.py
+++ CursoEmVideoMundo1/ex030.py
@@ -2,3 +2,8 @@
 Crie um programa que leia um número inteiro e mostre na tela se ele é par ou ímpar
 
 """
+numero = int(input("Informe um número inteiro: "))
+if (numero%2)==0:
+    print("O número {}, é PAR!".format(numero))
+else:
+    print("O número {}, é IMPAR!".format(numero))
