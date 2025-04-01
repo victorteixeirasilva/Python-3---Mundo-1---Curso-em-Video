151f1806f1c5bf063244c5f5d67768cadaef3bea
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 14:58:32 -0300

    ex038

--- CursoEmVideoMundo1/ex038.py
+++ CursoEmVideoMundo1/ex038.py
@@ -6,3 +6,11 @@ Escreva um programa que leia dois números inteiros e compare-os mostrando na te
 - Não existe valor maior, os dois são iguais.
 
 """
+numeroUm = int(input("Informe um número: "))
+numeroDois = int(input("Informe outro número: "))
+if numeroUm > numeroDois:
+    print("O primeiro valor é maior.")
+elif numeroDois > numeroUm:
+    print("O segundo valor é maior,")
+else:
+    print("Não existe valor maior, os dois são iguais.")
