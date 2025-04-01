e82532258752bd8bdbbaceecb3e0b9fb722cf729
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 17:06:38 -0300

    Copiando Exercicios

---
+++ CursoEmVideoMundo1/ex046.py
@@ -0,0 +1,5 @@
+"""
+Faça um programa que mostre na tela uma contagem regressiva para estouro de fogos o estouro de fogos de artíficio, indo
+de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+"""
---
+++ CursoEmVideoMundo1/ex047.py
@@ -0,0 +1,4 @@
+"""
+Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 a 50.
+
+"""
