0892b007f74ab1bea3d66c2b5cfb4849a9c861f8
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 19:17:12 -0300

    ex053

--- CursoEmVideoMundo1/ex053.py
+++ CursoEmVideoMundo1/ex053.py
@@ -2,3 +2,18 @@
 Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
 
 """
+frase = input("Digite uma frase: ")
+fraseSemEspacos = frase.upper().replace(" ", "").strip()
+fraseInvertida = ""
+palindromo = True
+for c in range(len(fraseSemEspacos)-1, -1, -1):
+    fraseInvertida += fraseSemEspacos[c]
+for c in range(0, len(fraseSemEspacos)):
+    if fraseSemEspacos[c] != fraseInvertida[c]:
+        palindromo = False
+if palindromo:
+    print("A frase ({}), é um palíndromo!".format(frase))
+else:
+    print("A frase ({}), não é um palíndromo!".format(frase))
+print(fraseSemEspacos)
+print(fraseInvertida)
