366c9c24ce84d28b825abb1acbf04b78581985cb
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 15:06:26 -0300

    ex039

--- CursoEmVideoMundo1/ex039.py
+++ CursoEmVideoMundo1/ex039.py
@@ -8,3 +8,15 @@ Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo 
 Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
 
 """
+import datetime
+anoDeNascimento = int(input("Informe seu ano de nascimento: "))
+anoAtual = int(datetime.date.today().year)
+idade = anoAtual-anoDeNascimento
+print("De acordo com seu ano de nascimento ({}), e o ano atual ({}). verificamos que você tem {}anos"
+      .format(anoDeNascimento, anoAtual, idade))
+if idade > 18:
+    print("Já passou do tempo do alistamento. Está {} anos atrasados.".format(idade-18))
+elif idade < 18:
+    print("Ainda vai se alistar ao serviço militar. Deve fazer isso em {} anos.".format(18-idade))
+else:
+    print("É hora de se alistar!")
