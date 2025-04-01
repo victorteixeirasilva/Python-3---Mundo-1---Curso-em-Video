0bf700298c6af5994fbf832353a94b0d7f42a374
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 11:07:40 -0300

    ex029

--- CursoEmVideoMundo1/ex029.py
+++ CursoEmVideoMundo1/ex029.py
@@ -6,3 +6,10 @@ Se ela ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado.
 A multa vai custar R$ 7,00 por cada Km acima do limite.
 
 """
+
+velocidade = int(input("Informe a velocidade do carro: "))
+if velocidade > 80:
+    print("Você foi multado! pois está acima do limite de velocidade")
+    print("O valor da multa é de R${:.2f}".format(7*(velocidade-80)))
+else:
+    print("Você estava dentro do limite!")
