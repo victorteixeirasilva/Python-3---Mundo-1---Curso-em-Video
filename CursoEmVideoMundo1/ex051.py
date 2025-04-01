f10f00971a2e98d00ed8e4b6e966f7261514e65a
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 18:19:24 -0300

    ex051

--- CursoEmVideoMundo1/ex051.py
+++ CursoEmVideoMundo1/ex051.py
@@ -3,4 +3,17 @@ Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final,
 progressão.
 
 """
+def mostrar_progressao(primeiro_termo, razao):
+    termos = []
+    for i in range(10):  # Calcula os 10 primeiros termos
+        termo = primeiro_termo + i * razao
+        termos.append(termo)
+    return termos
 
+# Exemplo de uso
+primeiro_termo = int(input("Digite o primeiro termo da PA: "))
+razao = int(input("Digite a razão da PA: "))
+
+progressao = mostrar_progressao(primeiro_termo, razao)
+print("Os 10 primeiros termos da PA são:")
+print(progressao)
