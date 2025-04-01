b887e9329ce9a3f7ef8f347cc3b103833ecf713c
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 18:15:22 -0300

    ex048

--- CursoEmVideoMundo1/ex048.py
+++ CursoEmVideoMundo1/ex048.py
@@ -3,3 +3,8 @@ Faça um programa que calcule a soma entre todos os números ímpares que são m
 intervalo de 1 até 500.
 
 """
+soma = 0
+for n in range(1, 501):
+    if (n%2) != 0:
+        soma += n
+print("A soma de todos os valores ímpares no intervalo de 1 até 500 é {}".format(soma))
