ce80fbc18dce4c8f27b5ecf0c112f4b2560af249
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 12:08:49 -0300

    ex035

--- CursoEmVideoMundo1/ex035.py
+++ CursoEmVideoMundo1/ex035.py
@@ -2,3 +2,17 @@
 Escreva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo
 
 """
+
+a = int(input("Informe o comprimento da primeira reta: "))
+b = int(input("Informe o comprimento da segunda reta: "))
+c = int(input("Informe o comprimento da terceira reta: "))
+if (a + b) > c:
+    if (a + c) > b:
+        if (b + c) > a:
+            print("Pode formar um triângulo!")
+        else:
+            print("Não pode formar um triângulo!")
+    else:
+        print("Não pode formar um triângulo!")
+else:
+    print("Não pode formar um triângulo!")
