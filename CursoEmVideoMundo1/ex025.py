fd74521d7d3dcf255445e2aa1111153f8f580660
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-31 23:24:53 -0300

    ex025

--- CursoEmVideoMundo1/ex025.py
+++ CursoEmVideoMundo1/ex025.py
@@ -2,3 +2,6 @@
 Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
 
 """
+nome = input("Informe seu nome completo: ")
+resultado = "SILVA" in nome.upper()
+print("O nome {}, tem SILVA? {}".format(nome, resultado))
