e5f533c8d657e80b0242593786256eee159cd379
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 11:47:11 -0300

    ex033

--- CursoEmVideoMundo1/ex033.py
+++ CursoEmVideoMundo1/ex033.py
@@ -2,3 +2,27 @@
 Escreva um programa que leia três números e mostre qual é o maior e qual é o menor.
 
 """
+n1 = int(input("Informe um número: "))
+n2 = int(input("Informe um número: "))
+n3 = int(input("Informe um número: "))
+if n1 > n2:
+    if n1 > n3:
+        print("O número {} é o maior".format(n1))
+    else:
+        print("O número {} é o maior".format(n3))
+else:
+    if n2 > n3:
+        print("O número {} é o maior".format(n2))
+    else:
+        print("O número {} é o maior".format(n3))
+
+if n1 < n2:
+    if n1 < n3:
+        print("O número {} é o menor".format(n1))
+    else:
+        print("O número {} é o menor".format(n3))
+else:
+    if n2 < n3:
+        print("O número {} é o menor".format(n2))
+    else:
+        print("O número {} é o menor".format(n3))
