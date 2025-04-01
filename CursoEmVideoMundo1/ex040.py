94a065580c1bfd941d06cc5459c0ba48159119cc
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 15:32:30 -0300

    ex040

--- CursoEmVideoMundo1/ex040.py
+++ CursoEmVideoMundo1/ex040.py
@@ -7,3 +7,12 @@ média atingida:
 - Média 7.0 ou superior: APROVADO
 
 """
+n1 = float(input("Informe a primeira nota: "))
+n2 = float(input("Informe a segunda nota: "))
+media = (n1+n2)/2
+if media < 5.0:
+    print("Reprovado!")
+elif (media >= 5.0) and (media <= 6.9):
+    print("Recuperação!")
+else:
+    print("APROVADO!")
