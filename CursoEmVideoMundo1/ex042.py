b65125e2acd62df6695c23ed47b16b1ec0a1efc8
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 14:33:08 -0300

    Copiando Exercicios

---
+++ CursoEmVideoMundo1/ex042.py
@@ -0,0 +1,8 @@
+"""
+Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+
+- Equilátero: Todos os lados iguais
+- Isósceles: Dois lados iguais.
+- Escaleno: Todos os lados diferentes
+
+"""
