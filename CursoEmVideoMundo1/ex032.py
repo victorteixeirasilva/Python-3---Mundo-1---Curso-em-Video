61d9d38e05eb416f80e067d821d483a06c3c9697
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 11:27:44 -0300

    ex032

--- CursoEmVideoMundo1/ex032.py
+++ CursoEmVideoMundo1/ex032.py
@@ -2,3 +2,15 @@
 Faça um programa que leia o ano qualquer e mostre se ele é bissexto.
 
 """
+ano = int(input("Informe um ano: "))
+if (ano % 4)==0:
+    if(ano % 100)!=0:
+            print("O ano {} é BISSEXTO!".format(ano))
+    else:
+        if (ano % 400) == 0:
+            print("O ano {} é BISSEXTO!".format(ano))
+else:
+    if (ano % 400) == 0:
+        print("O ano {} é BISSEXTO!".format(ano))
+
+    print("O ano {} não é BISSEXTO!".format(ano))
