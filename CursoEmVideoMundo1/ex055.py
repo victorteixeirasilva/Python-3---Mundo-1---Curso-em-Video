5ca0b0c52aeff991c96da12688b2fa003ba9e8ba
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 19:33:56 -0300

    ex055

--- CursoEmVideoMundo1/ex055.py
+++ CursoEmVideoMundo1/ex055.py
@@ -2,3 +2,13 @@
 Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 
 """
+maiorPeso = 0.0
+menorPeso = 0.0
+for c in range(1, 6):
+    peso = float(input("Informe o peso: "))
+    if peso > maiorPeso:
+        maiorPeso = peso
+    elif peso < menorPeso or menorPeso == 0.0:
+        menorPeso = peso
+print("O maior peso informado foi {}Kg".format(maiorPeso))
+print("O menor peso informado foi {}Kg".format(menorPeso))
