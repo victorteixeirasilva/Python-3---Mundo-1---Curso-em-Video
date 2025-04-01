659189e39f0a8ed4af2cd94126e7dd65c6506c9f
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 18:11:39 -0300

    ex046

--- CursoEmVideoMundo1/ex046.py
+++ CursoEmVideoMundo1/ex046.py
@@ -3,3 +3,8 @@ Faça um programa que mostre na tela uma contagem regressiva para estouro de fog
 de 10 até 0, com uma pausa de 1 segundo entre eles.
 
 """
+import time
+for count in range(10, -1, -1):
+    print(count)
+    time.sleep(1)
+print("BUM! POOW! POPOP!")
