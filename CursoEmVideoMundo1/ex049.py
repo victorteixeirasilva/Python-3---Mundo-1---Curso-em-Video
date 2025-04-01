165b8d8a2a810f805f1b99c6a13309bc6e63edc1
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 18:17:06 -0300

    ex049

--- CursoEmVideoMundo1/ex049.py
+++ CursoEmVideoMundo1/ex049.py
@@ -2,3 +2,8 @@
 Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
 
 """
+number = int(input("Informe um número: "))
+print("*** TABUADA ***")
+for tabuada in range(1, 11):
+    print("{} X {} = {}".format(number, tabuada, number*tabuada))
+
