11e1a9ce9494dbee2c5762c468e53988e1443ca8
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 12:03:55 -0300

    ex034

--- CursoEmVideoMundo1/ex034.py
+++ CursoEmVideoMundo1/ex034.py
@@ -6,3 +6,11 @@ Para salários superiores a R$1250,00, calcule um aumento de 10%.
 Para os inferiores ou iguais, o aumento é de 15%.
 
 """
+
+salario = float(input("Qual o seu salário atual: R$"))
+if salario > 1250:
+    print("Para o salario R${}, o aumento será de 10%, sendo assim seu salário será R${}"
+          .format(salario, salario+(salario*0.1)))
+else:
+    print("Para o salario R${}, o aumento será de 15%, sendo assim seu salário será R${}"
+          .format(salario, salario+(salario*0.15)))
