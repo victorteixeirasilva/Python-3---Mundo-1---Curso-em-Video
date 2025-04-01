74e9bb19869ed2cae8984baa78220ad0ece014b7
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 19:24:11 -0300

    ex054

--- CursoEmVideoMundo1/ex054.py
+++ CursoEmVideoMundo1/ex054.py
@@ -3,3 +3,16 @@ Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre 
 a maioridade e quantas já são maiores.
 
 """
+from datetime import date
+maiores = 0
+menores = 0
+anoAtual = date.today().year
+for c in range(1, 8):
+    anoNascimento = int(input("Informe o ano de nascimento: "))
+    idade = anoAtual-anoNascimento
+    if idade >= 18:
+        maiores += 1
+    else:
+        menores += 1
+print("Das 6 pessoas {} são maiores.".format(maiores))
+print("Das 6 pessoas {} são menores.".format(menores))
