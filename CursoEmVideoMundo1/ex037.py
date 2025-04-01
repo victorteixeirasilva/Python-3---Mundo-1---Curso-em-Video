6d4a29fe4da4c545a8f10b313ef66c399779b37b
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-01 14:54:43 -0300

    ex037

--- CursoEmVideoMundo1/ex037.py
+++ CursoEmVideoMundo1/ex037.py
@@ -6,4 +6,17 @@ Escreva um programa que leia um número inteiro qualquer e peça para o usuário
 - 3 para hexadecimal
 
 """
-
+numero = int(input("Informe um inteiro: "))
+print("O número informado foi ({}).".format(numero))
+print("1 - para ver fazer a conversão para binário")
+print("2 - para ver fazer a conversão para octal")
+print("3 - para ver fazer a conversão para hexadecimal")
+opcao = int(input("Escolha uma das 3 opções: "))
+if opcao==1:
+    print(bin(numero))
+elif opcao==2:
+    print(oct(numero))
+elif opcao==3:
+    print(hex(numero))
+else:
+    print("Opção inválida!")
