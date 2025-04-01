b8ffd1e98a5a39f7f69fdfc67fd51156aeede20b
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-31 23:17:53 -0300

    ex024

--- CursoEmVideoMundo1/ex024.py
+++ CursoEmVideoMundo1/ex024.py
@@ -2,3 +2,5 @@
 Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
 
 """
+cidade = input("Informe um nome de uma cidade: ")
+print("A cidade começa com 'Santo' : {}".format(cidade.upper().startswith("SANTO")))
