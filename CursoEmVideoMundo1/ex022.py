da7985987c877e309caf0ae3ed7aea5542209583
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-31 22:47:29 -0300

    ex022

--- CursoEmVideoMundo1/ex022.py
+++ CursoEmVideoMundo1/ex022.py
@@ -10,3 +10,11 @@ Crie um programa que leia o nome completo de uma pessoa e mostre
 -> Quantas letras tem o primeiro nome.
 
 """
+
+nomeCompleto = input("Digite seu nome completo: ")
+print("Nome com todas as letras maiúsculas {}".format(nomeCompleto.upper()))
+print("Nome com todas as letras minúsculas {}".format(nomeCompleto.lower()))
+print("Quantidade de letras presentes no nome, sem considerar os espaços é {}"
+      .format(len(nomeCompleto.replace(" ", ""))))
+print("Quantidade de Letras presentes no primeiro nome é {}"
+      .format(len(nomeCompleto.split()[0])))
