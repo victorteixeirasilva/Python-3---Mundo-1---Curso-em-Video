2318440a5b80a1513af695f2a34c7337460ce73c
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-31 23:28:35 -0300

    ex026

--- CursoEmVideoMundo1/ex026.py
+++ CursoEmVideoMundo1/ex026.py
@@ -8,3 +8,8 @@ Faça um programa que leia uma frase pelo teclado e mostre:
 -> Em que posição ela aparece a última vez.
 
 """
+
+frase = input("Digite algo: ")
+print("A letra (A) aparece ({}x)".format(frase.upper().count("A")))
+print("A letra (A) aparece primeiro na posição ({})".format(frase.upper().find("A")))
+print("A letra (A) aparece por último na posição ({})".format(frase.upper().rfind("A")))
