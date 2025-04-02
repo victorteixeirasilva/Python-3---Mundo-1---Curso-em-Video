7ea28d60a2d37025f5bf412557c47a65c561cca7
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-02 14:48:25 -0300

    Adicionando Exercicios

---
+++ CursoEmVideoMundo1/ex064.py
@@ -0,0 +1,3 @@
+# Crie um programa que leia varios números inteiros pelo teclado.
+# O programa só pode parar quando o usuário digitar o valor 999, que é a condição de parada.
+# No final mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
