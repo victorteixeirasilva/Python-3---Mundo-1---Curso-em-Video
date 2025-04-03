80c0edc40745dd181a7b21a5e5eba3af9a9d87cf
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-03 19:31:02 -0300

    ex073

--- CursoEmVideoMundo1/ex073.py
+++ CursoEmVideoMundo1/ex073.py
@@ -4,4 +4,28 @@
 # A) Apenas os 5 primeiros colocados.
 # B) Os últimos 4 colocados da tabela.
 # C) Uma lista com os times em ordem alfabética.
-# D) Em qual posição na tabela está o time da Chapecoense.
+# D) Em qual posição na tabela está o time da São Paulo.
+colocados = ("Desconsiderar", "Fortaleza", "Juventude", "Vasco", "Cruzeiro", "Grêmio", "Bragantino", "Ceará", "Corinthians", "Flamengo", "Internacional", "Bahia", "São Paulo", "Sport", "Botafogo", "Palmeiras", "Atlético-MG", "Santos", "Mirassol", "Fluminense", "Vitória")
+print()
+print("_" * 63)
+print("|{:^61}|".format("Os 5 primeiros colocados!"))
+print("_" * 63)
+print("|{:^30}|{:^30}|".format("Colocação", "Time"))
+print("_" * 63)
+for c in range(1, 6):
+    print("|{:^30}|{:^30}|".format(c, colocados[c]))
+print("_" * 63)
+print()
+print("_" * 63)
+print("|{:^61}|".format("Os últimos 4 colocados da tabela!"))
+print("_" * 63)
+print("|{:^30}|{:^30}|".format("Colocação", "Time"))
+print("_" * 63)
+for c in range(20, 16, -1):
+    print("|{:^30}|{:^30}|".format(c, colocados[c]))
+print("_" * 63)
+print()
+print("Lista de times, ordem alfabetica ({})".format(sorted(colocados)))
+print()
+print(colocados.index("São Paulo"))
+
