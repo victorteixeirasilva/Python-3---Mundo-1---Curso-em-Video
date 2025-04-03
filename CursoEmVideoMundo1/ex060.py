2585d1cd63be0f08d52e9179fd6ed188accfeafc
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-02 22:34:11 -0300

    ex060

--- CursoEmVideoMundo1/ex060.py
+++ CursoEmVideoMundo1/ex060.py
@@ -2,3 +2,20 @@
 #
 # Ex:
 # 5! = 5x4x3x2x1 = 120
+numero = int(input("Informe um número: "))
+resultado = ""
+resultado += str(numero)
+resultado += "!"
+resultado += " = "
+resultado += str(numero)
+contador = numero
+resultadoMultiplicação = numero
+while contador>1:
+    resultadoMultiplicação *= contador-1
+    resultado += "x"
+    resultado += str(contador-1)
+    contador = contador - 1
+resultado += " = "
+resultado += str(resultadoMultiplicação)
+print(resultado)
+
