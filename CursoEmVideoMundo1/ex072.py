f14223fd1a2223b9a2c6e74f72878ce987899088
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-03 19:11:59 -0300

    ex072

--- CursoEmVideoMundo1/ex072.py
+++ CursoEmVideoMundo1/ex072.py
@@ -1,2 +1,5 @@
 # Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
 # Seu programa devera ler um número de pelo teclado (entre 0 e 20) e mostrá-la por extenso.
+contExtenso = ("Zero", "Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Onze", "Doze", "Treze", "Quatorze", "Quinze", "Dezesseis", "Dezessete", "Dezoito", "Dezenove", "Vinte")
+numero = int(input("Informe um número de 0 até 20: "))
+print(contExtenso[numero])
