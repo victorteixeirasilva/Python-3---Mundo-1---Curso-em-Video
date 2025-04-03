2142bfad1b315b0e6cccc80a2ad1c839d9f25b58
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-02 22:41:19 -0300

    ex061

--- CursoEmVideoMundo1/ex061.py
+++ CursoEmVideoMundo1/ex061.py
@@ -1,2 +1,10 @@
 # Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão
 # usando a estrutura while.
+termo = int(input("Informe o primeiro termo da PA: "))
+razão = int(input("Informe a razão da PA: "))
+print("PA: {} ".format(termo), end = " ")
+contador = 1
+while contador<10:
+    termo = termo+razão
+    print(" {} ".format(termo), end = "")
+    contador += 1
