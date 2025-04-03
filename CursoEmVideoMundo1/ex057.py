299d1ad1f0c807ad44851377833bf3e0b3adfcc9
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-02 21:38:21 -0300

    ex057

--- CursoEmVideoMundo1/ex057.py
+++ CursoEmVideoMundo1/ex057.py
@@ -1,2 +1,13 @@
 # Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'.
 # Caso esteja errado, peça a digitação novamente, até ter um valor correto.
+from operator import truediv
+
+sexoValido = True
+while sexoValido:
+    sexo = str(input("Informe o sexo (F/M): "))
+    if sexo.upper() == "M":
+        print("MASCULINO")
+        sexoValido = False
+    elif sexo.upper() == "F":
+        print("FEMININO")
+        sexoValido = False
