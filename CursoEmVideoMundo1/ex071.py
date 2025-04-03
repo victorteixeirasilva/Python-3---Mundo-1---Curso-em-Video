b0686ae903ec4ad9871ba1558d04a0a96d16733c
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-03 15:40:43 -0300

    ex071

--- CursoEmVideoMundo1/ex071.py
+++ CursoEmVideoMundo1/ex071.py
@@ -2,3 +2,26 @@
 # (número inteiro) e o programa vai informar quantas céculas de cada valor serão entregues.
 #
 # OBS: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+print("=" * 30)
+print("{:^30}".format("BANCO CEV"))
+print("=" * 30)
+valor = int(input("Informe o valor que quer sacar? R$"))
+total = valor
+céd = 50
+totCéd = 0
+while True:
+    if total >= céd:
+        total -= céd
+        totCéd += 1
+    else:
+        if totCéd > 0:
+            print(F"Total de {totCéd} cédular de R${céd}")
+        if céd == 50:
+            céd = 20
+        elif céd == 20:
+            céd = 10
+        elif céd == 10:
+            céd = 1
+        totCéd = 0
+        if total == 0:
+            break
