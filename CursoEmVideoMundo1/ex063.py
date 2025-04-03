849ab8baf9db97de7bb87d37a09aab6d8a109e0a
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-02 23:00:14 -0300

    ex063

--- CursoEmVideoMundo1/ex063.py
+++ CursoEmVideoMundo1/ex063.py
@@ -3,3 +3,14 @@
 #
 # Ex:
 # 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
+elementos = int(input("Informe quantos elementos quer ver da sequencia Fibonacci: "))
+elementoAnterior = 1
+elementoAtual = 2
+contadorDeElementos = 4
+print("0 -> 1 -> 1 -> 2", end=" ")
+while contadorDeElementos < elementos:
+    temp = elementoAtual + elementoAnterior
+    print("-> {}".format(temp), end = " ")
+    elementoAnterior = elementoAtual
+    elementoAtual = temp
+    contadorDeElementos += 1
