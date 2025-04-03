07d07b689b2d5f502d5c9d3a9a151d6e72ebd868
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-03 19:39:03 -0300

    ex074

--- CursoEmVideoMundo1/ex074.py
+++ CursoEmVideoMundo1/ex074.py
@@ -1,2 +1,16 @@
 # Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
-# Depois disso, mostre a listagem de números gerados e também indique o manor e o maior valor que estão na tupla.
+# Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
+import random
+numeros = (random.randint(1, 11), random.randint(1,11), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10))
+print(numeros)
+maiorNumero = 0
+menorNumero = 0
+for c in numeros:
+    if c >= maiorNumero:
+        maiorNumero = c
+    elif c < menorNumero:
+        menorNumero = c
+    if menorNumero == 0:
+        menorNumero = c
+print(f"Menor número da tupla {menorNumero}")
+print(f"Maior número da tupla {maiorNumero}")
