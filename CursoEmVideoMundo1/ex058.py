c69de6f38c827542694ab2c061fdb97d511b2275
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-02 21:48:40 -0300

    ex058

--- CursoEmVideoMundo1/ex058.py
+++ CursoEmVideoMundo1/ex058.py
@@ -1,3 +1,19 @@
 # Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um número entre 0 e 10.
 # Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites
 # foram necessários para vencer.
+
+import random
+
+print("O computador está pensando em um número de 1 a 5...")
+numero = random.randrange(1, 11)
+qtdPalpites = 1
+jogoContinua = True
+while jogoContinua:
+    numeroSugestão = int(input("Tente adivinhar o número: "))
+    if numero == numeroSugestão:
+        print("Você acertou!, e deu {} palpites".format(qtdPalpites))
+        jogoContinua = False
+    else:
+        print("Você Errou!")
+        qtdPalpites += 1
+
