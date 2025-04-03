5ee9053b79f320c809d60f36281082cc6249e71d
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-03 16:57:50 -0300

    Anotando Exercicios

---
+++ CursoEmVideoMundo1/ex072.py
@@ -0,0 +1,2 @@
+# Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
+# Seu programa devera ler um número de pelo teclado (entre 0 e 20) e mostrá-la por extenso.
---
+++ CursoEmVideoMundo1/ex073.py
@@ -0,0 +1,7 @@
+# Crie uma tupla preenchida com os primeiros 20 colocados da tabela do Campeonato Brasileiro de Futebol, na ordem
+# de colocação, Depois mostre:
+#
+# A) Apenas os 5 primeiros colocados.
+# B) Os últimos 4 colocados da tabela.
+# C) Uma lista com os times em ordem alfabética.
+# D) Em qual posição na tabela está o time da Chapecoense.
