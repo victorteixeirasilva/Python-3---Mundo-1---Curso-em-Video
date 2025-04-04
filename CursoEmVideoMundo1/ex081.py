adf58a2c02c1d6a081d71f91731e032a285216bf
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-04 16:46:13 -0300

    ex081

--- CursoEmVideoMundo1/ex081.py
+++ CursoEmVideoMundo1/ex081.py
@@ -4,3 +4,25 @@
 # A) Quantos números foram digitados.
 # B) A lista de valores ordenada em ordem decrescente.
 # C) Se o valor 5 foi digitado e se está ou não na lista.
+numeros = []
+count = 0
+resposta5 = "não foi"
+while True:
+    numero = int(input("Informe um número: "))
+    if numero == 5:
+        resposta5 = "foi"
+    numeros.append(numero)
+    while True:
+        r = str(input("Deseja continuar [S/N]: ")).strip().upper()[0]
+        if r in "SN":
+            break
+    if r in "Nn":
+        break
+print("-=-" * 10)
+print(f"Foram digitados {len(numeros)} números.")
+print("-=-" * 10)
+numeros.sort(reverse=True)
+print(numeros)
+print("-=-" * 10)
+print(f"O valor 5 {resposta5} digitado.")
+print("-=-" * 10)
