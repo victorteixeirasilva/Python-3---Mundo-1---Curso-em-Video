853377222c7243e9fb94c598c7f04d781847c947
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-04 19:18:22 -0300

    ex085

--- CursoEmVideoMundo1/ex085.py
+++ CursoEmVideoMundo1/ex085.py
@@ -3,3 +3,17 @@ Crie um programa onde o usuário possa digitar sete valores numéricos e cadastr
 que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 
 """
+par = []
+impar = []
+numeros = [par, impar]
+for c in range(1, 8):
+    numero = int(input("Informe um número: "))
+    if numero % 2 == 0:
+        numeros[0].append(numero)
+    else:
+        numeros[1].append(numero)
+print(numeros)
+numeros[0].sort()
+numeros[1].sort()
+print(numeros[0])
+print(numeros[1])
