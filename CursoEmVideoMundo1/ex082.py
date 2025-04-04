1241d94b01edc851e671cccca14ea35067909db4
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-04 16:57:48 -0300

    ex082

--- CursoEmVideoMundo1/ex082.py
+++ CursoEmVideoMundo1/ex082.py
@@ -4,3 +4,27 @@
 # e os valores impares digitados, respectivamente.
 #
 # Ao final, mostre o conteúdo das três listas geradas.
+numeros = []
+numerosPares = []
+numerosImpares = []
+while True:
+    numero = int(input("Informe um número: "))
+    numeros.append(numero)
+    while True:
+        r = str(input("Deseja continuar [S/N]")).strip().upper()[0]
+        if r in "SN":
+            break
+    if r in "N":
+        break
+for n in numeros:
+    if (n % 2) == 0:
+        numerosPares.append(n)
+    else:
+        numerosImpares.append(n)
+print("-=-" * 20)
+print("Números Informados: {}".format(numeros))
+print("-=-" * 20)
+print("Números Pares: {}".format(numerosPares))
+print("-=-" * 20)
+print("Números Impares: {}".format(numerosImpares))
+print("-=-" * 20)
