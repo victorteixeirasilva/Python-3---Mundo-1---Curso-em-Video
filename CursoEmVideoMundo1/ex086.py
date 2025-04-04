c9e24ab8906c61a478c34c286f4d118deafd5ca5
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-04 19:37:34 -0300

    ex086

--- CursoEmVideoMundo1/ex086.py
+++ CursoEmVideoMundo1/ex086.py
@@ -4,3 +4,16 @@ Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lid
 No final mostre na tela, com a formatação correta.
 
 """
+coluna1 = []
+coluna2 = []
+coluna3 = []
+matriz = [coluna1, coluna2, coluna3]
+# matriz = [linhas]
+for c in range(0, 3):
+    for d in range(0, 3):
+        numero = int(input("Informe um número: "))
+        matriz[c].append(numero)
+for linha in matriz:
+    print()
+    for numero in linha:
+        print(f"|{numero}|", end="")
