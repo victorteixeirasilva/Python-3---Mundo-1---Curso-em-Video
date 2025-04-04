160cb43dd71c5579d0f75b483a34957c120a4ff5
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-04 19:54:23 -0300

    ex088

--- CursoEmVideoMundo1/ex088.py
+++ CursoEmVideoMundo1/ex088.py
@@ -4,3 +4,17 @@ perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 a 60 par
 cadastrando tudo em uma lista composta. OBS: O número não pode se repetir dentro de um mesmo jogo.
 
 """
+import random
+numeroDeJogos = int(input("Informe quantos jogos deseja fazer: "))
+jogos = []
+for a in range(0, numeroDeJogos):
+    jogo = []
+    for b in range(1, 7):
+        while True:
+            numero = random.randrange(1, 61)
+            if numero not in jogo:
+                break
+        jogo.append(numero)
+    jogos.append(jogo[:])
+    jogo.clear()
+print(jogos)
