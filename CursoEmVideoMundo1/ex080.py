3e3726d998c48d3a2ab48c0992398ac3b83a5704
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-04 16:35:57 -0300

    ex080

--- CursoEmVideoMundo1/ex080.py
+++ CursoEmVideoMundo1/ex080.py
@@ -2,3 +2,16 @@
 # já na posição correta de inserção (sem usar o short()).
 #
 # No final, mostre a lista ordenada na tela.
+numeros = []
+for c in range(1, 6):
+    numero = int(input("Informe um número: "))
+    if len(numeros) == 0:
+        numeros.append(numero)
+    else:
+        for i, c in enumerate(numeros):
+            if numero < c and numero not in numeros:
+                numeros.insert(i, numero)
+            elif numero > c and numero not in numeros:
+                numeros.append(numero)
+print("-=-" * 20)
+print(numeros)
