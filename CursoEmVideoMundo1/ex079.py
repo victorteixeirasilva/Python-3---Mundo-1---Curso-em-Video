f420e36b86168a4d7f14890159f8003dde710efb
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-04 16:06:46 -0300

    ex079

--- CursoEmVideoMundo1/ex079.py
+++ CursoEmVideoMundo1/ex079.py
@@ -1,3 +1,17 @@
 # Crie um programa onde o usuário pode digitar vários valores numéricos e cadastre-os em uma lista.
 # Caso o númerico já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os
 # valores únicos digitados, em ordem crescente.
+numeros = []
+while True:
+    numero = int(input("Informe um número: "))
+    if numero not in numeros:
+        numeros.append(numero)
+    else:
+        print("Número já adicionado, não vou adicionar")
+    while True:
+        desejaContinuar = str(input("Deseja continuar [S/N]: ")).strip().upper()[0]
+        if desejaContinuar == "S" or desejaContinuar == "N":
+            break
+    if desejaContinuar == "N":
+        break
+print(sorted(numeros))
