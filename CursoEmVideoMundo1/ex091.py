ef1a5215d857c16bda8dc6c2439de35896786da4
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-05 20:52:52 -0300

    ex091

--- CursoEmVideoMundo1/ex091.py
+++ CursoEmVideoMundo1/ex091.py
@@ -4,3 +4,12 @@ esses resultados em um dicionário. No final, coloque esse dicionário em ordem,
 que o vencedor tirou o maior número no dado.
 
 """
+import random
+import time
+import operator
+dicionario = {"jogador1":0, "jogador2":0, "jogador3":0, "jogador4":0}
+for i in range(0, 4):
+    dicionario["jogador{}".format(i)] = random.randrange(1,6)
+    print(f"Jogador {i+1}, jogando dado...")
+    time.sleep(1)
+print(sorted(dicionario.items(), key=operator.itemgetter(1),reverse=True))
