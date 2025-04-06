02f8324b3987011449d60f3e196d48af49270598
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-06 19:29:55 -0300

    ex101

--- CursoEmVideoMundo1/ex101.py
+++ CursoEmVideoMundo1/ex101.py
@@ -4,3 +4,14 @@ nascimento de uma pessoa retornando um valor literal indicando se uma pessoa tem
 opcional ou obrigatório nas eleições.
 
 """
+import datetime
+
+
+def voto(a):
+    idade = datetime.datetime.now().year - a
+    if 16 <= idade < 18:
+        return "Voto Opcional"
+    elif idade >= 18:
+        return "Voto Obrigatório"
+
+print(voto(2003))
