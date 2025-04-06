9d2ebabb9db545538683a5c050fa6e2806ee1271
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-06 12:08:36 -0300

    ex099

--- CursoEmVideoMundo1/ex099.py
+++ CursoEmVideoMundo1/ex099.py
@@ -4,3 +4,13 @@ Faça um programa que tenha uma função chamada maior(), que recebe vários par
 Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 
 """
+def maior(* num):
+    tamanho = len(num)*2
+    print("-=-"*tamanho)
+    print(num)
+    lista = sorted(num).copy()
+    print(f"O maior número informado é {lista[len(lista)-1]}")
+    print("-=-"*tamanho)
+
+
+maior(5, 10, 4, 12, 23)
