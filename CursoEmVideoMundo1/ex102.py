9a37be23b6ac54e0f460fcdc07c3fe19129550a7
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-06 20:31:04 -0300

    ex102

--- CursoEmVideoMundo1/ex102.py
+++ CursoEmVideoMundo1/ex102.py
@@ -10,4 +10,22 @@
 # 	:param n: O número a ser calculado.
 # 	:param show: (opcional) Mostra ou não a conta.
 # 	:return: O valor do Fatorial de um número n.
+import math
 
+
+def fatorial(n, show=False):
+    """
+    -> Calcula o Fatorial de um número.
+	:param n: O número a ser calculado.
+	:param show: (opcional) Mostra ou não a conta.
+	:return: O valor do Fatorial de um número n.
+    """
+    resultado = 1
+    for i in range(n, 0, -1):
+        if show:
+            print(f"{i}", end=" x " if i > 1 else " = ")
+        resultado *= i
+    return resultado
+
+
+print(fatorial(5))
