99a46c62d90e34a3f331a6b3876a6004ee35931e
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-05 22:31:02 -0300

    ex093

--- CursoEmVideoMundo1/ex093.py
+++ CursoEmVideoMundo1/ex093.py
@@ -5,3 +5,15 @@ quantidade de gols feitos em cada partida. No final, tudo isso será guardado em
 dicionário, incluindo o total de gols feitos durante o campeonato.
 
 """
+jogador = dict()
+jogador["nome"] = input("Informe o nome do jogador: ")
+quantidadeDePartidaJogadas = int(input("Informe a quantidade de partida jogadas: "))
+partidas = []
+for i in range(0, quantidadeDePartidaJogadas):
+    partidas.append(int(input(f"Informe a quantidade de gols feitos na partida {i}: ")))
+totGols = 0
+for i, p in enumerate(partidas):
+    totGols += p
+jogador["partidas"] = partidas
+jogador["total de gols"] = totGols
+print(jogador)
