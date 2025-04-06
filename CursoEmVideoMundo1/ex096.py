691d60ebdd4ff059d79ad339e08aab8e6e20dc59
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-06 11:07:51 -0300

    ex096

---
+++ CursoEmVideoMundo1/ex096.py
@@ -0,0 +1,12 @@
+"""
+Faça um programa que tenha um função chamada
+área(), que recebe as dimensões de um terreno
+retangular (largura e comprimento) e mostre a
+área do terreno.
+
+"""
+def área(largura, comprimento):
+    print(f"A área do terrone de largura={largura} e comprimento={comprimento}, é {largura*comprimento}m^2")
+
+
+área(10,20)
