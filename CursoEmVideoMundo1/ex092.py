f64d2f1937d064b805b9522b9dbe0ff4b9532912
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-05 21:11:34 -0300

    ex092

--- CursoEmVideoMundo1/ex092.py
+++ CursoEmVideoMundo1/ex092.py
@@ -3,4 +3,18 @@ Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadas
 (com idade em) um dicionário se por acaso a CTPS for diferente de Zero, o dicionário
 também recebera o ano de contratação e o salário. Calcule e acrescente, além da idade
 com quantos anos a pessoa vai se aposentar.
+
 """
+import datetime
+funcionario = dict()
+funcionario["nome"] = input("Informe o nome do funcionário: ")
+anoDeNascimento = int(input("Informe o ano de nascimento: "))
+anoAtual = datetime.date.today().year
+idade = anoAtual - anoDeNascimento
+funcionario["idade"] = idade
+funcionario["CTPS"] = int(input("Informe a carteira de tabalho: "))
+if funcionario["CTPS"] != 0:
+    funcionario["Ano De Contratação"] = int(input("Ano de contratação: "))
+    anosTrabalhados = anoAtual - funcionario["Ano De Contratação"]
+    funcionario["Idade Para se Aposentar"] = (55-anosTrabalhados)+funcionario["idade"]
+print(funcionario)
