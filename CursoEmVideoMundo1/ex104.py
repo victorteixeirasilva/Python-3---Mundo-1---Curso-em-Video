56ed87a0c8183b9c153e9a6db985bc7643515393
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-06 20:48:38 -0300

    ex104

--- CursoEmVideoMundo1/ex104.py
+++ CursoEmVideoMundo1/ex104.py
@@ -2,3 +2,16 @@
 # à função input do Python, só que fazendo validação para aceitar apenas um valor numérico.
 #
 # Ex: leiaint('Digite um n')
+def leiaint(msg):
+    while True:
+        valor = input(msg)
+        valorInt = 0
+        if valor.isnumeric():
+            valorInt = int(valor)
+            if valorInt.is_integer():
+                return valorInt
+        else:
+            print("ERRO: Digite um valor inteiro!")
+
+variavel = leiaint("Informe um int: ")
+print(variavel)
