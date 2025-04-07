d2acaba36d50ee8e09f41ec9c70b6eb339cbd4f1
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-07 13:15:00 -0300

    ex106

--- CursoEmVideoMundo1/ex106.py
+++ CursoEmVideoMundo1/ex106.py
@@ -3,3 +3,31 @@
 # se encerrará.
 #
 # OBS: use cores.
+c = (
+    "\033[n",       # 0 - sem cores
+    "\033[0;30;41", # 1 - vermelho
+)
+def ajuda(com):
+    help(com)
+
+
+def titulo(msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end=" ")
+    print("~" * tam)
+    print(f"  {msg}")
+    print("~" * tam)
+    print(c[0], end=" ")
+
+
+
+# Programa
+comando = ""
+while True:
+    titulo("SISTEMA DE AJUDA PyHelp", 1)
+    comando = str(input("Função ou Biblioteca > "))
+    if comando.upper().strip() == "FIM":
+        break
+    else:
+        ajuda(comando)
+    titulo("ATÉ LOGO!")
