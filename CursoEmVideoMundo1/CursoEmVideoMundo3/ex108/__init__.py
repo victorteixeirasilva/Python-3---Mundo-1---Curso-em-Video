8844b67a5bf157ef84583428280ae4c18b9b2ea9
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-07 14:18:56 -0300

    ex108

---
+++ CursoEmVideoMundo1/CursoEmVideoMundo3/ex108/__init__.py
@@ -0,0 +1,7 @@
+# Adapte o código do desafio 107, criando uma função adicional chamada moeda() que consiga mostrar os valores
+# como um valor monetário formatado.
+import moeda
+print(moeda.moeda(moeda.dobro(500)))
+print(moeda.moeda(moeda.metade(500)))
+print(moeda.moeda(moeda.aumentar(500, 50)))
+print(moeda.moeda(moeda.diminuir(500, 50)))
