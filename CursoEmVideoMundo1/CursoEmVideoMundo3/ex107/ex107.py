0e4d3122979b0150dfbd2ee82ddb5ea3d133f310
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-07 14:15:29 -0300

    ex107

---
+++ CursoEmVideoMundo1/CursoEmVideoMundo3/ex107/ex107.py
@@ -0,0 +1,8 @@
+# Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade()
+#
+# Faça também um programa que importe esse módulo e use algumas funções.
+import moeda
+print(moeda.dobro(500))
+print(moeda.metade(500))
+print(moeda.aumentar(500, 50))
+print(moeda.diminuir(500, 50))
